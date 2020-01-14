92a40556ef238a221d7e0ab5d51a038446a66c59
Darlan Damasio da Silva <darlandamasio@tecgraf.puc-rio.br>
2020-01-14 15:40:16 -0300

    Inclusão dos dados no mongo.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,7 @@
 // Importação de bibliotecas
 const { Router } = require('express');
+const axios = require('axios');
+const dev = require('./models/dev');
 
 // Declaração de constantes
 const routes = Router();
@@ -20,10 +22,32 @@ routes.get('/', (request, response) => {
     return response.json({ message: 'Hello Omnstack' });
 });
 
-// Cadastro de usuário
-routes.post('/users', (request, response) => {
-    console.log(request.body);
-    return response.json({ message: `Usuário ${request.body.nome} cadastrado com sucesso.` });
+// Cadastro de um dev
+routes.post('/devs', async (request, response) => {
+    const { github_username, techs, latitude, longitude } = request.body;
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    // Usando a destruturação, caso a variável nome seja vazia o valor da mesma será preenchido pela variável login
+    const { name = login, avatar_url, bio } = apiResponse.data;
+    // Map percorre um array
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+    }
+
+    const newDev = await dev.create({
+        github_username, 
+        name, 
+        avatar_url, 
+        bio, 
+        techs: techsArray, 
+        location
+    });
+
+    // console.log(name, bio, avatar_url);
+
+    return response.json(newDev);
 });
 
 module.exports = routes;
