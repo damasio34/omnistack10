825f066f808ef0a64e0375335fed685fc53648f2
Darlan Damasio da Silva <darlandamasio@tecgraf.puc-rio.br>
2020-01-14 14:07:10 -0300

    Coneção com o mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
-// Instanciando o express
+// Importação de bibliotecas
 const express = require('express');
+const mongoose = require('mongoose');
+
+// Instanciando o express
 const app = express();
+
+// Conexão com o mongoDB
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-30lsp.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // `use` significa que será válido ara todas as rotas da aplicação
 // Habilitando o formato json
 app.use(express.json());
 
 /**
+ * 
  * Métodos HTTP: GET, POST, PUT, DELETE
  * 
  * Tipos de parâmetros:
@@ -14,7 +24,10 @@ app.use(express.json());
  * Query Params: request.query (filtros, ordenação, paginação, ...)
  * Route Params: request.params (identificar um recurso na alteração ou remoção)
  * Body: request.body (dados para criação ou alteração de um registro)
+ * 
  */
+
+ // MongoDB não relaciona
 
 // URL root
 app.get('/', (request, response) => {
