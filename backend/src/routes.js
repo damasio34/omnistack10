a7978b2c9b52f24efcc92b5de4c9e525edf61838
Darlan Damasio da Silva <darlandamasio@tecgraf.puc-rio.br>
2020-01-14 17:14:01 -0300

    Implementação do search

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,7 @@
 // Importação de bibliotecas
 const { Router } = require('express');
-const axios = require('axios');
-const dev = require('./models/dev');
+const DevController = require('./controllers/DevControllers');
+const SearchController = require('./controllers/SearchController');
 
 // Declaração de constantes
 const routes = Router();
@@ -22,32 +22,11 @@ routes.get('/', (request, response) => {
     return response.json({ message: 'Hello Omnstack' });
 });
 
+// Lista de devs
+routes.get('/devs', DevController.index);
 // Cadastro de um dev
-routes.post('/devs', async (request, response) => {
-    const { github_username, techs, latitude, longitude } = request.body;
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-    // Usando a destruturação, caso a variável nome seja vazia o valor da mesma será preenchido pela variável login
-    const { name = login, avatar_url, bio } = apiResponse.data;
-    // Map percorre um array
-    const techsArray = techs.split(',').map(tech => tech.trim());
-
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude]
-    }
-
-    const newDev = await dev.create({
-        github_username, 
-        name, 
-        avatar_url, 
-        bio, 
-        techs: techsArray, 
-        location
-    });
-
-    // console.log(name, bio, avatar_url);
-
-    return response.json(newDev);
-});
+routes.post('/devs', DevController.store);
+// Busca de devs
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
