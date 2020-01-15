2de1b3b1d9d273b87fc7b5ac86bd2c0f9e3f1a98
Darlan Damasio da Silva <darlandamasio@tecgraf.puc-rio.br>
2020-01-15 09:01:52 -0300

    Aplicação da abordagem do react

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
+// useState: função do react para alteração de estado
+import React, { useState } from 'react';
 
-// Componente: Um bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
-// Propriedade: São parâmetros de um componente
-// Estado: 
-
-import Header from './Header';
+// Componente: Um bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação;
+// Propriedade: São parâmetros de um componente;
+// Estado: Informações mantidas pelo componente (lembrar de imutabilidade);
 
 function App() {
+    const [counter, setCounter] = useState(0);
+
+    function incrementCounter() {
+        setCounter(counter + 1);
+    }
+
     return (
         // Fragment: É uma tag sem informação, utilizada apenas para marcação, ex: <></>
         <>
-            <Header title="Dashboard"></Header>
-            <Header title="Titulo 2"></Header>
-            <Header title="Titulo 4"></Header>
+            <h1>Contador: {counter}</h1>
+            <button onClick={incrementCounter}>Incrementar</button>
         </>
     );
 }
